5988dc436aab3009a96be3862858a16e832134a2
JWeez <jwellstx@gmail.com>
2020-03-03 01:22:37 -0600

    formatting and comments added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,17 +2,21 @@ var App = {
     topics: ["cheese", "snl", "motorcycles", "dogs", "funny", "confused", "explosion", "shaq", "football", "uno", "random"],
     appendMode: false,
     startApp: function () {
+        // Create initial buttons
         this.createButtons();
 
+        // When submit button pressed, add new topic and recreate buttons
         $(document).on("click", "#addTopic", function () {
             App.topics.push($('#newTopic').val().trim());
             App.createButtons();
         });
+        // When someone types in search box and presses enter instead of submit, force addtopic to be clicked
         $(document).on("keyup", "#newTopic", function (event) {
             if (event.key !== "Enter") return;
             $('#addTopic').click();
             event.preventDefault();
         });
+        // Once category is pushed, color the catorey button, query giphy api and display sill images
         $(document).on("click", ".category", function () {
             if (!App.appendMode) {
                 $('.category').css({ "background-color": "#008000" });
@@ -20,9 +24,11 @@ var App = {
             $(this).css({ "background-color": "black" });
             App.generateStillGifs($(this));
         });
+        // When someone clicks on the image, animate it, or vice versa
         $(document).on("click", ".myImage", function () {
             App.animateGifs($(this));
         });
+        // Specify if you want to append more gifs to the end of the page, or overwrite them.  Set color for pushed buttons
         $(document).on("click", "#appendMode", function () {
             if (App.appendMode) {
                 App.appendMode = false;
@@ -34,12 +40,15 @@ var App = {
                 $(this).css({ "background-color": "#008000" });
             }
         });
+        // When add to favorite button is pressed, copy the parent and append it to favorites
         $(document).on("click", ".favButton", function () {
             App.addToFavorite($(this).parent());
         });
+        // When remove from favorite is pressed, just delete element
         $(document).on("click", ".rmFavButton", function () {
             $(this).parent().remove();
         });
+        // When clear is pressed, empty out the main content section but keep favorites
         $(document).on("click", "#clear", function () {
             $('.results').empty();
             $('.category').css({ "background-color": "#008000" });
@@ -48,6 +57,7 @@ var App = {
         // $('.favorites').append(document.cookie);
     },
     createButtons: function () {
+        // Based on contents of this.topic, create list of category buttons, including user specified ones
         var mainContent = $(".maincontent");
         $('.category').remove();
         $.each(this.topics, function (index, value) {
@@ -60,6 +70,9 @@ var App = {
         });
     },
     generateStillGifs: function (obj) {
+        // Do the giphy API call using ajax, parse response, build element for each image, including data- 
+        // attributes and then append them in rows/columns in the main content div.
+        // Includes tite, rating, category, image with both still animated options, and an add to favorite button.
         // API key LoR3cMaOF474wZ6NCThSPCVKtFNYbUNu
         var results = $('.results');
         var offset = Math.floor(Math.random() * 100);
@@ -76,10 +89,11 @@ var App = {
             var newRow = $("<div class='row'>");;
             for (var i = 0; i < response.data.length; i++) {
                 
-                newDiv = $("<div class='col-md-3' style='float: left'>");
+                newDiv = $("<div class='col-md-4' style='float: left'>");
 
                 newDiv.append("<strong>Title:</strong> " + response.data[i].title + "<br>");
                 newDiv.append("<strong>Rating:</strong> " + response.data[i].rating + "<br>");
+                newDiv.append("<strong>Category:</strong> " + obj.text() + "<br>");
                 var newGif = $('<img>');
                 newGif.addClass("myImage");
                 newGif.attr("src", response.data[i].images.fixed_height_still.url);
@@ -93,7 +107,8 @@ var App = {
                 newDiv.append(newFav);
 
                 newRow.append(newDiv);
-                if ((i-2) % 3 === 0 ) {
+                // Since our grid is setup in columns of 3, every 3 entires append the current divs and create a new row
+                if ((i-2) % 3 === 0 ) { // 2 (includes 0/1/2 div) 5 (includes 3/4/5 div) 8 (includes 6/7/8 div)
                     results.append(newRow);
                     newRow = $("<div class='row'>");
                 }
@@ -101,6 +116,7 @@ var App = {
         });
     },
     animateGifs: function (obj) {
+        // When image clicked, switch src and flip flop data-state
         var state = obj.attr("data-state");
         var still = obj.attr("data-still");
         var animate = obj.attr("data-animate");
@@ -117,18 +133,20 @@ var App = {
     addToFavorite: function (obj) {
         var newObj = obj.clone();
         var childImg = newObj.children("img");
-        // if clicked image is animated, set the favorites to still image
+        // If clicked image is animated, set the favorites to still image
         if (childImg.attr("data-state") === "animate") {
             childImg.attr("data-state", "still");
             childImg.attr("src", childImg.attr("data-still"));
         }
+        // Append copied image from results, modify remove button
         $('.favorite').append(newObj);
         $('.favorite button').text("Remove from favorites!");
         $('.favorite button').removeClass("favButton");
         $('.favorite button').addClass("rmFavButton");
+        $('.favorite div').removeClass("col-md-4").addClass("col-md-12");  /* cloned data source is a 3 column into a 1 column */
 
-        document.cookie = "cookie1=test; expires=Fri, 19 Jun 2020 20:47:11 UTC; path=/";
-        console.log(document.cookie);
+        // document.cookie = "cookie1=test; expires=Fri, 19 Jun 2020 20:47:11 UTC; path=/";
+        // console.log(document.cookie);
     },
 }
 
